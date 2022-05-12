54759da30e32bab1812f75f45f277da1fa9bc74d
arijunhao <jh.wang@nus.edu.sg>
2022-05-12 20:36:04 +0800

    First Commit

---
+++ data.js
@@ -0,0 +1,188 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-erc-level-1-main-lobby",
+      "name": "ERC Level 1 Main Lobby",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "yaw": 2.177641601576246,
+        "pitch": -0.07392693990720645,
+        "fov": 1.4134061960355204
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.783099324906674,
+          "pitch": 0.0672525168641922,
+          "rotation": 0.7853981633974483,
+          "target": "1-erc-level-1-tembusu-tree-garden"
+        },
+        {
+          "yaw": -0.8821646697880503,
+          "pitch": 0.07905351098195723,
+          "rotation": 3.141592653589793,
+          "target": "2-erc-basement-1-study-area"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.796223100244088,
+          "pitch": -0.26488899137071087,
+          "title": "Welcome!",
+          "text": "Psst.. Here in the ERC, you will find a vast array of IT and multimedia facilities, and lots of study spots!"
+        },
+        {
+          "yaw": 1.8215625513090385,
+          "pitch": -0.03687743797491727,
+          "title": "Yale-NUS College",
+          "text": "This part of the map is still not unlocked yet. So explorers, complete the ERC journey first!"
+        },
+        {
+          "yaw": -1.1149641450279582,
+          "pitch": 0.03997704835373028,
+          "title": "Scale Model of UTown",
+          "text": "I mean... who doesn't like miniature towns?"
+        }
+      ]
+    },
+    {
+      "id": "1-erc-level-1-tembusu-tree-garden",
+      "name": "ERC Level 1 Tembusu Tree Garden",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.260120661040146,
+          "pitch": 0.1083159576938435,
+          "rotation": 5.497787143782138,
+          "target": "0-erc-level-1-main-lobby"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -0.34045912697062697,
+          "pitch": 0.2804118296587248,
+          "title": "Tembusu Trees",
+          "text": "Who would have thought that you can find huge heritage trees in the middle of a building?"
+        },
+        {
+          "yaw": 1.8354608424639363,
+          "pitch": 0.3862358511426969,
+          "title": "Resting Spots",
+          "text": "Take a restful break in this scenic garden!&nbsp;"
+        }
+      ]
+    },
+    {
+      "id": "2-erc-basement-1-study-area",
+      "name": "ERC Basement 1 Study Area",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1440,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.994235133930065,
+          "pitch": -0.3402570076419771,
+          "rotation": 5.497787143782138,
+          "target": "0-erc-level-1-main-lobby"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.9293217683739172,
+          "pitch": -0.14537895857922578,
+          "title": "Starbucks Cafe",
+          "text": "Time to get a cup of Joe!"
+        },
+        {
+          "yaw": -2.3433099323731934,
+          "pitch": -0.10314486254452504,
+          "title": "F&amp;B Vending Machines",
+          "text": "Wouldn't it be nice to snack and study at the same time? Hang on, in moderation of course!"
+        },
+        {
+          "yaw": 0.15940505553340856,
+          "pitch": -0.009141528606413374,
+          "title": "Study Space",
+          "text": "Packed with students, especially during the Reading Week!"
+        },
+        {
+          "yaw": -0.5183903853222951,
+          "pitch": -0.19042741489819903,
+          "title": "UTown Residence",
+          "text": "This part of the map is coming soon.&nbsp;&nbsp;"
+        }
+      ]
+    }
+  ],
+  "name": "ERC Tour May 2022",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
